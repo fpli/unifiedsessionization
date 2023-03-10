62d4fd491b3bb082ff63277e094aec5c2cb899c2
xiaoding <xiaoding@ebay.com>
2023-03-10 13:29:54 +0800

    upgrade from V1.11 to 1.13

--- src/main/java/com/ebay/epic/flink/connector/kafka/factory/FlinkKafkaConsumerFactory.java
+++ src/main/java/com/ebay/epic/flink/connector/kafka/factory/FlinkKafkaConsumerFactory.java
@@ -19,8 +19,38 @@ public class FlinkKafkaConsumerFactory {
         this.configWrapper = configWrapper;
     }
 
-    public <T> SojFlinkKafkaConsumer<T> get(KafkaDeserializationSchema<T> deserializer) {
-        SojFlinkKafkaConsumer<T> flinkKafkaConsumer = new SojFlinkKafkaConsumer<>(
+    // Deperecated due to Flink 1.12+ has fixed the issue https://issues.apache.org/jira/browse/FLINK-19750
+    //    public <T> SojFlinkKafkaConsumer<T> get(KafkaDeserializationSchema<T> deserializer) {
+    //        SojFlinkKafkaConsumer<T> flinkKafkaConsumer = new SojFlinkKafkaConsumer<>(
+    //                configWrapper.getKafkaConsumerConfig().getTopics(),
+    //                deserializer,
+    //                configWrapper.getKafkaConsumerConfig().getProperties());
+    //        if (configWrapper.getOutOfOrderlessInMin() > 0) {
+    //            log.error("if init timestampand watermarks:{}", configWrapper.getOutOfOrderlessInMin());
+    //            flinkKafkaConsumer.assignTimestampsAndWatermarks(
+    //                    WatermarkStrategy
+    //                            .forBoundedOutOfOrderness(Duration.ofMinutes(configWrapper.getOutOfOrderlessInMin()))
+    //                            .withTimestampAssigner(new TrackingEventTimestampAssigner())
+    //                            .withIdleness(Duration.ofMinutes(configWrapper.getIdleSourceTimeout())));
+    //        } else {
+    //            log.error("else init timestamp and watermarks:{}", configWrapper.getOutOfOrderlessInMin());
+    //        }
+    //        String fromTimestamp = configWrapper.getFromTimestamp();
+    //        if (fromTimestamp.equalsIgnoreCase("earliest")) {
+    //            flinkKafkaConsumer.setStartFromEarliest();
+    //        } else if (Long.parseLong(fromTimestamp) == 0) {
+    //            flinkKafkaConsumer.setStartFromLatest();
+    //        } else if (Long.parseLong(fromTimestamp) > 0) {
+    //            flinkKafkaConsumer.setStartFromTimestamp(Long.parseLong(fromTimestamp));
+    //        } else {
+    //            throw new IllegalArgumentException("Cannot parse fromTimestamp value");
+    //        }
+    //
+    //        return flinkKafkaConsumer;
+    //    }
+
+    public <T> FlinkKafkaConsumer<T> get(KafkaDeserializationSchema<T> deserializer) {
+        FlinkKafkaConsumer<T> flinkKafkaConsumer = new FlinkKafkaConsumer<>(
                 configWrapper.getKafkaConsumerConfig().getTopics(),
                 deserializer,
                 configWrapper.getKafkaConsumerConfig().getProperties());
