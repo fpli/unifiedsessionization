eb96c6a563c9ee992f0ebbd761c8acea3115cd5c
cmao <cmao@ebay.com>
2023-06-08 14:40:48 +0800

    emit traffic source when session end

--- common/src/main/java/com/ebay/epic/soj/common/model/trafficsource/TrafficSourceDetails.java
+++ common/src/main/java/com/ebay/epic/soj/common/model/trafficsource/TrafficSourceDetails.java
@@ -2,6 +2,9 @@ package com.ebay.epic.soj.common.model.trafficsource;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 public class TrafficSourceDetails {
     private String trafficSourceLevel3;
@@ -9,4 +12,26 @@ public class TrafficSourceDetails {
     private int mpxChnlId;
     private String pageName;
     private String referer;
+
+    public Map<String, String> toMap() {
+        if (trafficSourceLevel3 == null) {
+            return null;
+        } else {
+            Map<String, String> trafficSourceMap = new HashMap<>();
+            trafficSourceMap.put("traffic_source_level3", trafficSourceLevel3);
+            if (rotid > 0) {
+                trafficSourceMap.put("rotid", String.valueOf(rotid));
+            }
+            if (mpxChnlId > 0) {
+                trafficSourceMap.put("mpx_chnl_id", String.valueOf(mpxChnlId));
+            }
+            if(pageName != null) {
+                trafficSourceMap.put("page_name", pageName);
+            }
+            if (referer != null) {
+                trafficSourceMap.put("referer", referer);
+            }
+            return trafficSourceMap;
+        }
+    }
 }
